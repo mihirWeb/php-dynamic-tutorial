24b66431bd3316893e1622ab34ec72641fff4ec7
mihirWeb <mihirpratapsingh166@gmail.com>
2024-08-10 02:03:48 +0530

    dynamic tutorial

---
+++ views/contact.view.php
@@ -0,0 +1,12 @@
+<?php require 'partials/head.php' ?>
+
+<div class="min-h-full">
+  
+<?php require 'partials/navbar.php' ?>
+<?php require 'partials/banner.php' ?>
+
+</div>
+
+    
+</body>
+</html>
